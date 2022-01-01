2f2911fcddedc4eaf5687e434476eaa32261e656
trswork <87744214+trswork@users.noreply.github.com>
2022-01-01 01:11:40 -0500

    update

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,16 @@
+router.post('/', (req, res) => {
+    // check the session
+    if (req.session) {
+      Comment.create({
+        comment_text: req.body.comment_text,
+        post_id: req.body.post_id,
+        // use the id from the session
+        user_id: req.session.user_id
+      })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+          console.log(err);
+          res.status(400).json(err);
+        });
+    }
+  });
